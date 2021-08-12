465e7afba2bf160571d78b14a762df192ebfdb38
Raquel Uribe <raquel.uribe.sierra@gmail.com>
2021-08-12 08:55:57 -0500

    Archivos añadidos

---
+++ Iterador.js
@@ -0,0 +1,14 @@
+function crearIterador (arreglo){
+    var siguienteIndice = 0;
+    return{
+        next: function(){
+            return siguienteIndice < arreglo.lenght ?
+                {value: arreglo[siguienteIndice++], done: false} :
+            {done: true};
+                }
+    }
+}
+var it= crearIterador(['yo', 'ya']);
+console.log(it.next().value);
+console.log(it.next().value);
+console.log(it.next().done);
